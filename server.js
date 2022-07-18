4536f04eee3c5f8a700f384d76c9c2f52bdee0a0
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-07-18 12:54:58 -0700

    query the database to test the connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const mysql = require('mysql2');
 const express = require('express');
+const { binaryExpression } = require('@babel/types');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,7 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        //your Mysql password
+        password: 'Mysql-0892',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
